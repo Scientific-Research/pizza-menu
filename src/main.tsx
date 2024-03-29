655fd0230e0bf44f0a31417d4b65764d3a543e81
Maximilian <scientificresearch309@gmail.com>
2024-03-29 12:45:47 +0100

    I reused the Pizza component as Pizza function in such a way that I just copied and pasted it.

--- src/main.tsx
+++ src/main.tsx
@@ -7,5 +7,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
     <Pizza />
+    {/* To reuse the component, it is just enough to copy and paste the compnent Pizza several times! */}
+    <Pizza />
+    <Pizza />
   </React.StrictMode>
 );

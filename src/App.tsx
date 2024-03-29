1c44fb5d99736c36ef7048ee8e095197162ac905
Maximilian <scientificresearch309@gmail.com>
2024-03-29 12:23:14 +0100

    the Pizza function as Pizza component is displaying now on the Browser!

--- src/App.tsx
+++ src/App.tsx
@@ -9,4 +9,9 @@ export default function App() {
   );
 }
 
-
+// NOTE: two important rules in React, when we write Components as function:
+// 1) The name of function must start with Uppercase letter!
+// 2) The function has to return some markup for example in form of JSX or even nothing => return null
+export const Pizza = () => {
+  return <h2>Pizza</h2>;
+};

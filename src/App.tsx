165cb1f2685c9c943ed7e27830daa2668827f479
Maximilian <scientificresearch309@gmail.com>
2024-04-02 23:15:47 +0200

    I added a text under our Menu text

--- src/App.tsx
+++ src/App.tsx
@@ -126,6 +126,10 @@ export const Menu = () => {
     <>
       <main className="menu">
         <h2>Our menu</h2>
+        <p>
+          Authentic Italian cuisine. 6 creative dishes to choose from. All from
+          our stone oven, all organic, all delicious.
+        </p>
         {/* Instead of doing && Operator, we do both section using ternary operator */}
         {pizzaData.length > 0 ? (
           <ul className="pizzas">
